2669d570a324fbc6bc7b43bd6e0080bc6558e35e
Helen Ye <00yhelen@gmail.com>
2018-01-16 09:43:28 -0500

    Draw rectangles around detected objects

--- src/Main.java
+++ src/Main.java
@@ -10,50 +10,50 @@ import stuyvision.gui.VisionGui;
 public class Main {
     public static void main(String[] args) {
         ModuleRunner runner = new ModuleRunner(5);
-        //processImages(runner);
-        processCamera(runner);
+        processImages(runner);
+        // processCamera(runner);
         VisionGui.begin(args, runner);
     }
-    
+
     public static File[] getFiles(String path) {
-    		path = System.getProperty("user.dir") + path;
-    		if (System.getProperty("os.name").toLowerCase().contains("windows")) {
-                path = path.replace('/', '\\');
-            }
-    		File directory = new File(path);
-    		File[] directoryListing = directory.listFiles();
-    		
-    		return directoryListing;
+        path = System.getProperty("user.dir") + path;
+        if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+            path = path.replace('/', '\\');
+        }
+        File directory = new File(path);
+        File[] directoryListing = directory.listFiles();
+
+        return directoryListing;
     }
-    
+
     public static void processImages(ModuleRunner runner) {
-    		String imagesDirectory =  "/pics/";
-    		
-    		System.out.println("Getting images from " + imagesDirectory);
-    		
-    		File[] imgs = getFiles(imagesDirectory);
-    		Mat frame = new Mat();
-    		
-    		for (int i = 0; i < imgs.length && i < 16; i++) {
-                String path = System.getProperty("user.dir") + imagesDirectory + imgs[i].getName();
-                System.out.println(imgs[i].getName() + " - " + path);
-                Vision v = new Vision();
-                
-                ImageCaptureSource img = new ImageCaptureSource(path);
-                img.readFrame(frame);
-            
-                v.run(frame);
-                runner.addMapping(img, v);
-            }
+        String imagesDirectory = "/pics/";
+
+        // System.out.println("Getting images from " + imagesDirectory);
+
+        File[] imgs = getFiles(imagesDirectory);
+        Mat frame = new Mat();
+
+        for (int i = 0; i < imgs.length && i < 5; i++) {
+            String path = System.getProperty("user.dir") + imagesDirectory + imgs[i].getName();
+            System.out.println(imgs[i].getName() + " - " + path);
+            Vision v = new Vision();
+
+            ImageCaptureSource img = new ImageCaptureSource(path);
+            img.readFrame(frame);
+
+            v.run(frame);
+            runner.addMapping(img, v);
+        }
     }
-    
+
     public static void processCamera(ModuleRunner runner) {
-    	DeviceCaptureSource camera = new DeviceCaptureSource(0);
-    	Mat frame = new Mat();
-    	camera.readFrame(frame);
-    	
-    	Vision v = new Vision();
-    	runner.addMapping(camera, v);
+        DeviceCaptureSource camera = new DeviceCaptureSource(0);
+        Mat frame = new Mat();
+        camera.readFrame(frame);
+
+        Vision v = new Vision();
+        runner.addMapping(camera, v);
     }
-    
+
 }
--- src/Vision.java
+++ src/Vision.java
@@ -2,6 +2,10 @@ import java.util.ArrayList;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
@@ -9,80 +13,78 @@ import org.opencv.imgproc.Imgproc;
 import stuyvision.VisionModule;
 import stuyvision.gui.IntegerSliderVariable;
 
+public class Vision extends VisionModule {
+    public IntegerSliderVariable minLightness = new IntegerSliderVariable("Min Lightness", 142, 0, 255);
+    public IntegerSliderVariable maxLightness = new IntegerSliderVariable("Max Lightness", 255, 0, 255);
 
-public class Vision extends VisionModule{
-	public IntegerSliderVariable minLightness = new IntegerSliderVariable("Min Lightness", 142, 0, 255);
-	public IntegerSliderVariable maxLightness = new IntegerSliderVariable("Max Lightness", 255, 0, 255);
-	
-	public IntegerSliderVariable minA = new IntegerSliderVariable("Min A", 86, 0, 255);
-	public IntegerSliderVariable maxA = new IntegerSliderVariable("Max A", 132, 0, 255);
-	
-	public IntegerSliderVariable minB = new IntegerSliderVariable("Min B", 157, 0, 225);
-	public IntegerSliderVariable maxB = new IntegerSliderVariable("Max B", 225, 0, 225);
-	
-	
-	public void run(Mat frame) {
+    public IntegerSliderVariable minA = new IntegerSliderVariable("Min A", 86, 0, 255);
+    public IntegerSliderVariable maxA = new IntegerSliderVariable("Max A", 132, 0, 255);
+
+    public IntegerSliderVariable minB = new IntegerSliderVariable("Min B", 157, 0, 225);
+    public IntegerSliderVariable maxB = new IntegerSliderVariable("Max B", 225, 0, 225);
+
+    public void run(Mat frame) {
         Mat filtered = filterImage(frame);
+        drawCube(frame, filtered);
         filtered.release();
-	}
-			
-	public Mat filterImage(Mat frame) {
-		System.out.println(frame);
-		if(hasGuiApp()) {
-			postImage(frame, "Image!");
-		}
-		
-		Mat filtered = new Mat();
-		Imgproc.cvtColor(frame, filtered, Imgproc.COLOR_BGR2Lab);
-		
-		ArrayList<Mat> channels = new ArrayList<Mat>();
-		Core.split(filtered, channels);
-		
-		if (hasGuiApp()) {
-			postImage(filtered, "Lightness Before Change Frame");
-		}
-		
+    }
+
+    public Mat filterImage(Mat frame) {
+        if (hasGuiApp()) {
+            postImage(frame, "Image!");
+        }
+
+        Mat filtered = new Mat();
+        Imgproc.cvtColor(frame, filtered, Imgproc.COLOR_BGR2Lab);
+
+        ArrayList<Mat> channels = new ArrayList<Mat>();
+        Core.split(filtered, channels);
+
+        if (hasGuiApp()) {
+            postImage(filtered, "Lightness Before Change Frame");
+        }
+
         Imgproc.medianBlur(channels.get(0), channels.get(0), 5);
-        
+
         // Filter channel by lightness
-        Core.inRange(channels.get(0), new Scalar(minLightness.value()), new Scalar(maxLightness.value()), channels.get(0));
+        Core.inRange(channels.get(0), new Scalar(minLightness.value()), new Scalar(maxLightness.value()),
+                channels.get(0));
         if (hasGuiApp()) {
             postImage(channels.get(0), "Lightness-Filtered Frame");
         }
-        
+
         // Dilate then erode
         Mat dilateKernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(5, 5));
         Imgproc.dilate(channels.get(0), channels.get(0), dilateKernel);
 
         Mat erodeKernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(7, 7));
         Imgproc.erode(channels.get(0), channels.get(0), erodeKernel);
-        
+
         // Filter channel by a
         Core.inRange(channels.get(1), new Scalar(minA.value()), new Scalar(maxA.value()), channels.get(1));
         if (hasGuiApp()) {
-        		postImage(channels.get(1), "A-Filtered Frame");
+            postImage(channels.get(1), "A-Filtered Frame");
         }
-        
+
         // Filter channel b
         Core.inRange(channels.get(2), new Scalar(minB.value()), new Scalar(maxB.value()), channels.get(2));
         if (hasGuiApp()) {
-        		postImage(channels.get(2), "B-Filtered Frame");
+            postImage(channels.get(2), "B-Filtered Frame");
         }
-        
+
         // AND the three channels into channels.get(0)
         Core.bitwise_and(channels.get(0), channels.get(1), filtered);
         Core.bitwise_and(filtered, channels.get(2), filtered);
-       
+
         Mat dilateKernelF = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(5, 5));
         Imgproc.dilate(filtered, filtered, dilateKernelF);
 
         Mat erodeKernelF = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(7, 7));
         Imgproc.erode(filtered, filtered, erodeKernelF);
 
-        if(hasGuiApp()) {
-        postImage(filtered, "Final Lab filtering");
+        if (hasGuiApp()) {
+            postImage(filtered, "Final Lab filtering");
         }
-
 
         // Release all Mats created
         for (int i = 0; i < channels.size(); i++) {
@@ -92,6 +94,44 @@ public class Vision extends VisionModule{
         erodeKernel.release();
 
         return filtered;
-      
-	}
+
+    }
+
+    public void drawCube(Mat original, Mat filtered) {
+        Mat drawn = original.clone();
+        ArrayList<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+        Imgproc.findContours(filtered, contours, new Mat(), Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        MatOfPoint2f approxCurve = new MatOfPoint2f();
+        ArrayList<Rect> rects = new ArrayList<Rect>();
+
+        for (int i = 0; i < contours.size(); i++) {
+            MatOfPoint2f contour2f = new MatOfPoint2f(contours.get(i).toArray());
+            double approxDistance = Imgproc.arcLength(contour2f, true) * 0.02;
+            Imgproc.approxPolyDP(contour2f, approxCurve, approxDistance, true);
+            MatOfPoint points = new MatOfPoint(approxCurve.toArray());
+
+            Rect rect = Imgproc.boundingRect(points);
+            rects.add(rect);
+
+            contour2f.release();
+        }
+
+        for (int i = 0; i < rects.size(); i++) {
+            Rect rect = rects.get(i);
+            Imgproc.rectangle(drawn, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height),
+                    new Scalar(0, 255, 0), 1);
+        }
+
+        if (hasGuiApp()) {
+            postImage(drawn, "Detected");
+        }
+
+        for (int i = 0; i < contours.size(); i++) {
+            contours.get(i).release();
+        }
+
+        approxCurve.release();
+        drawn.release();
+    }
 }
